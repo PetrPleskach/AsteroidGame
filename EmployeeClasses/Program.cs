4cf3c7de0f951fb9a381643da40e26886a9ec8cd
PetrPleskach <Pleskach_petr@mail.ru>
2021-01-15 20:41:15 +0300

    Добавлено решение, для Д3 №2

---
+++ EmployeeClasses/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
