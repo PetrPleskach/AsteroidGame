dcbd17ab1673bda3d81d2cf905668d2902b87025
PetrPleskach <Pleskach_petr@mail.ru>
2021-01-15 21:08:41 +0300

    Создан абстрактный класс Employee

---
+++ EmployeeClasses/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeClasses
+{
+    abstract class Employee
+    {
+        //Поля общие для всех работников
+        protected string name;
+        protected string surName;
+        protected int age;
+        protected string id;        
+        protected decimal salary;
+
+        protected Employee(string name, string surName, int age)
+        {
+            this.name = name;
+            this.surName = surName;
+            this.age = age;
+        }
+
+        /// <summary>
+        /// Абстрактный метод для расчёта зарплаты
+        /// </summary>
+        /// <returns></returns>
+        protected abstract decimal PaymentCalculation();
+    }
+}
