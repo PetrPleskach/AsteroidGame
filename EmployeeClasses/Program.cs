8c87b2045dc4174ba996a63fd76274c639c4ddf7
PetrPleskach <Pleskach_petr@mail.ru>
2021-01-16 22:19:09 +0300

    На базе абстрактного класса создан и заполнен массив работников

--- EmployeeClasses/Program.cs
+++ EmployeeClasses/Program.cs
@@ -5,15 +5,37 @@ namespace EmployeeClasses
 {
     class Program
     {
+        static string RandomStringGen()
+        {
+            Random random = new Random();
+            string str = "abcdefghijklmnopqrstuvwxyz";
+            StringBuilder sb = new StringBuilder(7);
+            int sym;
+            for (int i = 0; i < 7; i++)
+            {
+                sym = random.Next(0, str.Length);
+                sb.Append(str[sym]);
+            }
+            return sb.ToString();
+
+        }
         static void Main(string[] args)
         {
-            Console.OutputEncoding = Encoding.Unicode;            
+            Random random = new Random();
+            Console.OutputEncoding = Encoding.Unicode;
 
-            FixedPayEmploee emploee = new FixedPayEmploee("Иван", "Иванов", 22, 1000);
-            HourlyPayEmployee hourlyEmploee = new HourlyPayEmployee("Петр", "Петров", 33, 11);
+            Employee[] employes = new Employee[15];
+            for (int i = 0; i < employes.Length / 2; i++)
+                employes[i] = new FixedPayEmploee(RandomStringGen(), RandomStringGen(), 20 + i, random.Next(11111, 55555));
+            for (int i = employes.Length / 2; i < employes.Length; i++)
+                employes[i] = new HourlyPayEmployee(RandomStringGen(), RandomStringGen(), 20 + i, random.Next(111, 555));
 
-            Console.WriteLine(emploee);
-            Console.WriteLine(hourlyEmploee);
+            foreach (var item in employes)
+            {
+                Console.WriteLine(item);
+            }                      
+
+
 
             Console.ReadKey();
         }
