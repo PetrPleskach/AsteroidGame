5a6254227177beabece03d89fa31e14d01b48434
PetrPleskach <Pleskach_petr@mail.ru>
2021-01-19 14:01:12 +0300

    Добавлен класс Ship

---
+++ AsteroidGame/VisualObjects/Ship.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsteroidGame.VisualObjects
+{
+    class Ship : BaseVisualObject, ICollision
+    {
+        public event EventHandler ShipDestoyed;
+        public int Energy { get; set; } = 100;
+        public Rectangle Rect => new Rectangle(Position, Size);
+
+        public Ship(Point position, Point direction, Size size) :base(position, direction, size) { }
+
+        public override void Draw(Graphics graphics) => graphics.FillEllipse(Brushes.Wheat, Position.X, Position.Y, Size.Width, Size.Height);
+
+        public override void Update() { }
+
+        public void MoveUp()
+        {
+            if (Position.Y > 0) Position.Y -= Direction.Y;                
+        }
+
+        public void MoveDown()
+        {
+            if (Position.Y > Game.Height) Position.Y += Direction.Y;
+        }
+
+        public bool CheckCollision(ICollision obj)
+        {
+            bool isCollision = Rect.IntersectsWith(obj.Rect);
+            if(isCollision && obj is Asteroid asteroid)            
+                ChangeEnergy(-asteroid.Power);            
+            return isCollision;
+        }
+
+        public void ChangeEnergy(int valueToChange)
+        {
+            Energy += valueToChange;
+            if (Energy <= 0)
+                ShipDestoyed?.Invoke(this, EventArgs.Empty);
+        }
+
+
+    }
+}
